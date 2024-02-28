74a1ffd4f12ce2b1e240055b7206fe02ca11bc0c
trevorwdunn <trevorwdunn@icloud.com>
2024-02-27 21:09:06 -0600

    First commit. Just started this project.

---
+++ book.h
@@ -0,0 +1,23 @@
+// This is a header guard.
+#ifndef _BOOKS_H
+#define _BOOKS_H_
+
+#include <iostream>
+#include <string>
+
+class Book {
+private:
+  std::string bookTitle;
+  std::string bookDescription;
+  int rating;
+  double price;
+
+public:
+  void setTitle(std::string, bookTitle);
+  void setDescription(std::string, bookDescription);
+  void setRating(int, rating);
+  void setPrice(double, price);
+  void Print();
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+    Trevor Dunn
+    This is a project designed to help me work with classes in my Programming
+   Fundamentals II class. This is similar to, but not directly derived from,
+   code we are working on in class.
+
+*/
+
+#include "book.h"
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+int main() {
+  // this is a placeholder for preventing auto-formatting.
+
+  return 0;
+}
